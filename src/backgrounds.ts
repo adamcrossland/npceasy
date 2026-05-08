d57af0c807ac6c98deb613617b93b355618618bb
Adam Crossland <adam.crossland@gmail.com>
2026-05-08 12:39:42 -0400

    Add Backgrounds

---
+++ src/backgrounds.ts
@@ -0,0 +1,59 @@
+export interface Background {
+    name: string;
+    description: string;
+}
+
+export const Backgrounds: Background[] = [
+    {
+        name: 'Acolyte',
+        description: 'You have spent your life in the service of a temple to a specific god or pantheon of gods. You act as an intermediary between the realm of the holy and the mortal world, performing sacred rites and offering sacrifices in order to conduct worshipers into the presence of the divine.'
+    },
+    {
+        name: 'Charlatan',
+        description: 'You have always had a way with people. You are charming and persuasive, with a natural gift for deception. You have used these talents to swindle and con people throughout your life. You are a master of disguise and deception, and you use these skills to get what you want.'
+    },
+    {
+        name: 'Criminal',
+        description: 'You are an experienced criminal with a history of breaking the law. You may have been a burglar, assassin, gang member, or con artist. You know the skills necessary to commit crimes and evade the law, including lockpicking, sleight of hand, and stealth.'
+    },
+    {
+        name: 'Entertainer',
+        description: 'You thrive in front of an audience. You know how to entrance them, entertain them, and inspire them. Your talent has earned you fame and fortune in local communities and perhaps beyond. You perform for crowds in theaters, taverns, circuses, and street corners.'
+    },
+    {
+        name: 'Folk Hero',
+        description: 'You are a champion of the common people. You might have risked your life defending your village from invaders, rescued a village from a tyrannical lord, or stood up to a local tyrant. In any case, you are known as a defender of the simple folk.'
+    },
+    {
+        name: 'Guild Artisan',
+        description: 'You are a member of a guild of artisans and tradespeople. You have spent years perfecting your craft and rising through the ranks of your guild. You have a rank and responsibilities within your guild, and you are known for the quality of your work.'
+    },
+    {
+        name: 'Hermit',
+        description: 'You lived in seclusion, away from civilization and society at large—either in a sheltered community such as a monastery, or entirely alone. The time you spent in isolation gave you access to a unique and powerful perspective. You might have been searching for spiritual truth, fleeing persecution, or avoiding responsibilities and duties you could not face.'
+    },
+    {
+        name: 'Noble',
+        description: 'You understand wealth, power, and privilege. You might have been born into a noble family, earned a title through service to a king, or gained influence through a great deed. You are accustomed to the finer things in life and are used to having others defer to you.'
+    },
+    {
+        name: 'Outlander',
+        description: 'You grew up in the wilds, far from civilization and the comforts of town and technology. You are used to living off the land, of tracking down your own food, and of avoiding dangers of the wild. You have spent considerable time in the wilderness, and you know how to survive in it.'
+    },
+    {
+        name: 'Sage',
+        description: 'You spent years learning the lore of the multiverse. You scoured manuscripts, studied scrolls, and listened to the greatest experts telling of the secrets of the worlds. Your efforts have made you exceptionally learned, but also confined you to a life of study.'
+    },
+    {
+        name: 'Sailor',
+        description: 'Your life has been spent at sea. You are as comfortable on a ship as on land—perhaps more so. You have served as a member of a crew, whether on a merchant vessel, a ship of war, or a pirate ship. You have sailed across dangerous seas and seen things others have only dreamed of.'
+    },
+    {
+        name: 'Soldier',
+        description: 'War is in your blood. You trained as a youth, either in a local militia or in a lord\'s army. You saw combat while relatively young. Today the scars of past service are a reminder of the horrors of war, even if you are no longer an active soldier.'
+    },
+    {
+        name: 'Urchin',
+        description: 'You grew up on the streets alone, orphaned, and poor. You had no one to watch over you or to provide for you, so you learned to provide for yourself. You fought fiercely over food and kept a constant watch out for other urchins who might steal from you—or worse. You slept on rooftops and in alleyways, exposed to the elements, and endured sickness without the advantage of medicine or a place to recuperate.'
+    }
+];
